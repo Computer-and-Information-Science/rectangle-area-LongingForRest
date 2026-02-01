6386fd064f19ca9556376f1671bea1734cb67672
LongingForRest <ssfkkravitz@gmail.com>
2026-02-01 08:03:22 +0000

    I thought about using \n to just have 1 big output from a single cout but i thought that this way is much more readable and that is more valuable.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main() {
+  //constants
   const double length = 7.2;
   const double width = 1.3;
 
+  //varibles and computation
+double area = length * width;
+double perimeter = (length+width)*2;
+
+//output
+cout << "Rectangle properies:" << endl;
+cout << "Length = "<< length << endl;
+cout << "Width = " << width << endl;
+cout << "Area = "<< area << endl;
+cout << "Perimeter = " << perimeter << endl;
 
 }
